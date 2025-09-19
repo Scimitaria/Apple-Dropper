4a90557ce2ab34f33eb765546fc0073f68c5063b
Morgan Powers <morgan.c.powers@gmail.com>
2025-09-19 12:11:02 -0500

    apples drop

--- Assets/Scripts/AppleDropper.cs
+++ Assets/Scripts/AppleDropper.cs
@@ -9,13 +9,17 @@ public class AppleDropper : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        StartCoroutine(DropApples());
     }
 
     IEnumerator DropApples()
     {
+        spawnPosition.y = 4;
+        Quaternion rotation = Quaternion.Euler(0, 0, 0);
         while (true)
         {
+            spawnPosition.x = Random.Range(-8, 8);
+            Instantiate(ApplePrefabs[0], spawnPosition, rotation);
             yield return _waitForSeconds1;
         }
     }
