539243a6f3d8842a0e01b68e28b3ac96557a668b
Morgan Powers <morgan.c.powers@gmail.com>
2025-09-23 10:50:39 -0500

    difficulty increase added

--- Assets/Scripts/AppleDropper.cs
+++ Assets/Scripts/AppleDropper.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public class AppleDropper : MonoBehaviour
 {
-    private int difficulty = 1;
+    private int applesDropped = 0;
+    private float frequency = 1;
+    private int chance = 11;
     public GameObject[] ApplePrefabs;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,14 +17,21 @@ public class AppleDropper : MonoBehaviour
     {
         float y = transform.position.y;
         Quaternion rotation = Quaternion.Euler(0, 0, 0);
-        int selector = 0;
+        int selector;
         while (true)
         {
             selector = 0;
-            if (Random.Range(0, 9) == 1) selector = 1;
+
+            //increase difficulty
+            if (applesDropped % 20 == 0 && chance > 3) chance -= 1;
+            else if (applesDropped % 10 == 0) frequency += 0.5f;
+
+            if (Random.Range(1, chance) == 1) selector = 1;
             transform.position = new Vector2(Random.Range(-8, 8), y);
             Instantiate(ApplePrefabs[selector], transform.position, rotation);
-            yield return new WaitForSeconds(2/difficulty);
+            applesDropped += 1;
+
+            yield return new WaitForSeconds(2 / frequency);
         }
     }
 }
